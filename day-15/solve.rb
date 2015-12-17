40fbb6772120d0926c72886c1a451f687fededbd
Jamie Macey <jamie.git@tracefunc.com>
2015-12-17 11:44:47 -0800

    solve day 15

---
+++ day-15/solve.rb
@@ -0,0 +1,29 @@
+ingredients = File.readlines('input').map{|line|
+  line =~ /(.*): capacity (.*), durability (.*), flavor (.*), texture (.*), calories (.*)/
+  [
+    $2.to_i,
+    $3.to_i,
+    $4.to_i,
+    $5.to_i,
+  ]
+}
+
+max = 0
+
+100.downto(0) do |i|
+  (100-i).downto(0) do |j|
+    (100-i-j).downto(0) do |k|
+      l = 100-i-j-k
+      amounts = [i,j,k,l]
+      values = ingredients.zip(amounts).map{|ing, amt|
+        ing.map{|value| value * amt}
+      }
+      total_value = values.transpose.map{|properties| [properties.inject(&:+), 0].max }.inject(&:*)
+      if total_value > max
+        max = total_value
+      end
+    end
+  end
+end
+
+puts max
