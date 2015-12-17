5d185b4b797a0355b3eb3e5abba75c1438737df4
Jamie Macey <jamie.git@tracefunc.com>
2015-12-16 20:00:00 -0800

    combine scripts, try to reduce runtime

--- day-10/solve.rb
+++ day-10/solve.rb
@@ -1,6 +1,8 @@
 phrase = "3113322113"
 
-40.times do
+50.times do |i|
+  puts phrase.length if i == 40
+
   groups = phrase.scan(/((.)\2*)/).map(&:first)
   phrase = groups.map{|group| [group.length, group[0]] }.flatten.join
 end
--- day-10/solve2.rb
+++
@@ -1,8 +0,0 @@
-phrase = "3113322113"
-
-50.times do
-  groups = phrase.scan(/((.)\2*)/).map(&:first)
-  phrase = groups.map{|group| [group.length, group[0]] }.flatten.join
-end
-
-puts phrase.length
