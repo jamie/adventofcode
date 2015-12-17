9925ff2a792eb61767a2f4aef8059cae2779e3d9
Jamie Macey <jamie.git@tracefunc.com>
2015-12-16 19:55:15 -0800

    add some timing info

--- solve.rb
+++ solve.rb
@@ -1,9 +1,11 @@
 Dir["*/solve.rb"].each do |solver|
   path, script = solver.split("/")
   Dir.chdir path
+  start = Time.now
   solution = `ruby solve.rb`.chomp
   solution2 = File.exist?('solve2.rb') ? `ruby solve2.rb`.chomp : ''
+  stop = Time.now
   Dir.chdir '..'
 
-  puts "%s: %8s %8s" % [path, solution, solution2]
+  puts "%s: %8s %8s (%5.2fs)" % [path, solution, solution2, (stop-start)]
 end
