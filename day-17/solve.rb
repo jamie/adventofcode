644de4161048964d2658c910f9cfb12b211f0884
Jamie Macey <jamie.git@tracefunc.com>
2015-12-17 11:56:51 -0800

    solve day 17

---
+++ day-17/solve.rb
@@ -0,0 +1,10 @@
+containers = File.readlines('input').map(&:to_i).sort.reverse
+
+total = 0
+(1..containers.size).each do |count|
+  containers.combination(count).to_a.each do |combination|
+    total += 1 if combination.inject(&:+) == 150
+  end
+end
+puts total
+
