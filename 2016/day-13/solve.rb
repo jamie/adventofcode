a94a7e69cd6deb648b6d06def85685c4cc819d94
Jamie Macey <jamie.git@tracefunc.com>
2016-12-13 10:06:59 -0800

    day twelve

---
+++ 2016/day-13/solve.rb
@@ -0,0 +1,33 @@
+SEED = 1358
+TARGET = [31, 39]
+
+def wall?(x, y)
+  id = x*x + 3*x + 2*x*y + y + y*y + SEED
+  ones = id.to_s(2).split(//).select{|e|e=='1'}.size
+  ones % 2 == 1
+end
+
+seen = [[1,1]]
+search = [[1,1,0]]
+
+while search.any? do
+  x, y, steps = search.shift
+  [ [0,1],
+    [1,0],
+    [0,-1],
+    [-1,0]
+  ].each do |dx, dy|
+    xx, yy = x+dx, y+dy
+
+    if [xx, yy] == TARGET
+      puts steps+1
+      exit
+    end
+
+    next if xx < 0 || yy < 0
+    next if wall?(xx,yy)
+    next if seen.include?([xx,yy])
+    seen << [xx, yy]
+    search << [xx, yy, steps+1]
+  end
+end
