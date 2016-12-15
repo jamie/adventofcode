d56c3971e93ba5ee40be24442897118f71cf0586
Jamie Macey <jamie.git@tracefunc.com>
2016-12-14 21:10:54 -0800

    day fifteen, 93 points

---
+++ 2016/day-15/solve.rb
@@ -0,0 +1,17 @@
+discs = []
+File.readlines('input').each do |line|
+  fail unless line =~ /Disc #(.*) has (.*) positions; at time=0, it is at position (.*)./
+  discs << [$1, $2, $3].map(&:to_i)
+  discs[-1][2] += discs.size
+end
+
+time = 0
+loop do
+  break if discs.map(&:last).all? { |pos| pos.zero? }
+  discs.map! do |id, size, pos|
+    [id, size, (pos+1)%size]
+  end
+  time += 1
+end
+
+puts time
