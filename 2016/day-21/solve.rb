9b854cdb79d134190e7614dd0572fc8035875ace
Jamie Macey <jamie.git@tracefunc.com>
2016-12-21 12:03:28 -0800

    refactor for reuse in part two

--- 2016/day-21/solve.rb
+++ 2016/day-21/solve.rb
@@ -2,6 +2,18 @@ password = "abcdefgh"
 
 commands = File.readlines('input')
 
+def rotr(password, n)
+  n = n % password.length
+  return password if n.zero?
+  password[-n..-1] + password[0...-n]
+end
+
+def rotl(password, n)
+  n = n % password.length
+  return password if n.zero?
+  password[n..-1] + password[0...n]
+end
+
 commands.each do |command|
   case command
   when /swap position (\d+) with position (\d+)/
@@ -13,24 +25,22 @@ commands.each do |command|
   when /reverse positions (\d+) through (\d+)/
     i, j = $1.to_i, $2.to_i
     password[i..j] = password[i..j].reverse
-  when /rotate left (\d+) step/
-    i = $1.to_i
-    substr = password[0...i]
-    password.delete!(substr)
-    password << substr
-  when /rotate right (\d+) step/
-    i = $1.to_i
-    password = (password[-i..-1] + password)[0...-i] if i>0
   when /move position (\d+) to position (\d+)/
     i, j = $1.to_i, $2.to_i
     pass = password.split(//)
     char = pass.delete_at(i)
     pass.insert(j, char)
     password = pass.join
+  when /rotate left (\d+) step/
+    n = $1.to_i
+    password = rotl(password, n)
+  when /rotate right (\d+) step/
+    n = $1.to_i
+    password = rotr(password, n)
   when /rotate based on position of letter (.)/
     i = password.index($1)
-    j = 1 + i + (i < 4 ? 0 : 1)
-    j.times { password = (password[-1] + password)[0..-2] }
+    n = 1 + i + (i < 4 ? 0 : 1)
+    password = rotr(password, n)
   end
 end
 
