3a12ebad14cee44eaa6507837c6e97f84caba4b3
Jamie Macey <jamie.git@tracefunc.com>
2017-12-01 15:12:10 -0800

    toplevel flex solver

---
+++ solve.rb
@@ -0,0 +1,21 @@
+year, day = ARGV
+# If no year, assume latest
+year, day = nil, year if year.to_i < 2000
+year = Dir["20*"].last if year.nil?
+# If no day, make wildcard
+day = "0#{day}" if day && day.to_i < 10
+day = "*" if day.nil?
+
+Dir["#{year}/day-#{day}.rb"].each do |script|
+  path, file = script.split("/")
+
+  Dir.chdir path
+  start = Time.now
+  solution, solution2 = `ruby #{file}`.chomp.split("\n")
+  stop = Time.now
+  Dir.chdir '..'
+
+  puts "%s (%5.2fs)" % [script, (stop-start)]
+  puts "%10s" % solution
+  puts "%10s" % solution2
+end
