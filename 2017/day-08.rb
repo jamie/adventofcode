34799f400b69c74e2e91bc70c62493f8c1178aa5
Jamie Macey <jamie.git@tracefunc.com>
2017-12-09 11:12:49 -0800

    day 8

---
+++ 2017/day-08.rb
@@ -0,0 +1,18 @@
+input = File.readlines('input/08')
+
+registers = Hash.new{0}
+all_values = []
+
+input.each do |instruction|
+  fail instruction unless instruction =~ /(.+) (inc|dec) (-?\d+) if (.+) (<|>|<=|>=|==|!=) (-?\d+)/
+  reg, op, val, cond_reg, cond, cond_val = $1, $2, $3.to_i, $4, $5, $6.to_i
+
+  # Thanks, AoC, for choosing operators that match Ruby semantics
+  if registers[cond_reg].send(cond, cond_val)
+    val *= -1 if op == 'dec'
+    registers[reg] += val
+    all_values << registers[reg]
+  end
+end
+puts registers.values.max
+puts all_values.max
