1dcb296bbf71796f7517e1913811336e1345d170
Jamie Macey <jamie.git@tracefunc.com>
2017-12-10 10:17:56 -0800

    days 9 and 10

---
+++ 2017/day-09.rb
@@ -0,0 +1,34 @@
+stream = File.read('input/09').chomp.split(//)
+
+$garbage = []
+
+def parse_group(depth, stream)
+  score = depth
+
+  loop do
+    case stream.shift
+    when '{'; score += parse_group(depth+1, stream)
+    when '}'; break
+    when '<'; parse_garbage(stream)
+    when ','; # nop
+    else
+      exit "Unhandled char"
+    end
+  end
+  score
+end
+
+def parse_garbage(stream)
+  while stream[0] != '>'
+    if stream[0] == '!'
+      stream.shift; stream.shift 
+    else
+      $garbage << stream.shift
+    end
+  end
+  stream.shift
+end
+
+stream.shift
+puts parse_group(1, stream)
+puts $garbage.size
