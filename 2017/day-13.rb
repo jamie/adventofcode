f43dbe359159c6b97e2a8bd66d9e8d92d27b573c
Jamie Macey <jamie.git@tracefunc.com>
2017-12-14 13:46:19 -0800

    tidy up

--- 2017/day-13.rb
+++ 2017/day-13.rb
@@ -1,12 +1,12 @@
 input = File.readlines('input/13')
 
-cycle_scanners = input.map do |line|
+scanners = input.map do |line|
   line =~ /(\d+): (\d+)/
   [$1.to_i, $2.to_i, (($2.to_i) - 1)*2]
 end.sort_by(&:last)
 
 # Part 1
-severities = cycle_scanners.map{|depth, range, loop_size|
+severities = scanners.map{|depth, range, loop_size|
   if depth % loop_size == 0
     depth * range
   else
@@ -16,9 +16,8 @@ severities = cycle_scanners.map{|depth, range, loop_size|
 puts severities.inject(&:+)
 
 # Part 2
-inf = 5_000_000
-(0..inf).each do |delay|
-  next if cycle_scanners.any?{|depth, _range, loop_size|
+(0..50_000_000).each do |delay|
+  next if scanners.any?{|depth, _range, loop_size|
     (delay+depth) % loop_size == 0
   }
   puts delay
