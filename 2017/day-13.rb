c0016a1b0ee8bbdc1b9749438d583762b9f6bdda
Jamie Macey <jamie.git@tracefunc.com>
2017-12-14 13:43:41 -0800

    do the math, it's MUCH faster

--- 2017/day-13.rb
+++ 2017/day-13.rb
@@ -1,69 +1,26 @@
 input = File.readlines('input/13')
 
-class Scanner
-  attr_reader :pos, :range
-  def initialize(range)
-    @range = range
-    reset
-  end
-
-  def step
-    @pos += @dir
-    @dir = 1 if @pos == 0
-    @dir = -1 if @pos == @range-1
-  end
-
-  def reset
-    @pos = 0
-    @dir = 1
-  end
-end
-class StubScanner
-  def step
-  end
-
-  def reset
-  end
-
-  def pos
-    1
-  end
-end
-
-scanners = []
-input.each do |line|
+cycle_scanners = input.map do |line|
   line =~ /(\d+): (\d+)/
-  scanners[$1.to_i] = Scanner.new($2.to_i)
-end
-100.times do |i|
-  scanners[i] ||= StubScanner.new
-end
-
-def check_severity(delay, scanners, fail_fast=false)
-  severity = 0
-  depth = -1
-  while depth < 100 do
-    depth += 1
-    if scanners[depth] && scanners[depth].pos == 0
-      severity += depth * scanners[depth].range
-      return 1 if fail_fast
-    end
-    scanners.compact.each(&:step)
-  end
-  severity
-end
+  [$1.to_i, $2.to_i, (($2.to_i) - 1)*2]
+end.sort_by(&:last)
 
 # Part 1
-puts check_severity(0, scanners)
-# Part 2
-delay = 0
-scanners.each(&:reset)
-loop do
-  sev = check_severity(delay, scanners.map(&:dup), true)
-  break if sev.zero?
+severities = cycle_scanners.map{|depth, range, loop_size|
+  if depth % loop_size == 0
+    depth * range
+  else
+    0
+  end
+}
+puts severities.inject(&:+)
 
-  delay += 2
-  scanners.each(&:step)
-  scanners.each(&:step)
+# Part 2
+inf = 5_000_000
+(0..inf).each do |delay|
+  next if cycle_scanners.any?{|depth, _range, loop_size|
+    (delay+depth) % loop_size == 0
+  }
+  puts delay
+  break
 end
-puts delay
