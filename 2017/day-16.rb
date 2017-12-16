5a7c5c1a1abf105456878b75a834b81bc80e9ced
Jamie Macey <jamie.git@tracefunc.com>
2017-12-15 21:31:51 -0800

    day 16, unhappy with live cycle detection not working, so gave up

---
+++ 2017/day-16.rb
@@ -0,0 +1,31 @@
+input = File.read('input/16').chomp
+programs = 'abcdefghijklmnop'
+
+moves = input.split(",")
+
+cycle_size = 60 # By inspection, sigh.
+
+(1_000_000_000 % cycle_size).times do |ii|
+
+  moves.each do |move|
+    case move
+    when /s(\d+)/
+      x = $1.to_i
+      programs = programs[-x..-1] + programs[0...-x]
+    when /x(\d+)\/(\d+)/
+      i, j = $1.to_i, $2.to_i
+      programs[i], programs[j] = programs[j], programs[i]
+    when /p(.)\/(.)/
+      a, b = $1, $2
+      i, j = programs.index(a), programs.index(b)
+      programs[i], programs[j] = programs[j], programs[i]
+    else
+      puts "wtf move #{move}"
+    end
+  end
+
+  # Part 1
+  puts programs if ii.zero?
+end
+# Part 2
+puts programs
