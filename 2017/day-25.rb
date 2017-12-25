3afb96c4c37045fddecc25aedd16f72c8128ec51
Jamie Macey <jamie.git@tracefunc.com>
2017-12-25 12:28:49 -0800

    day 25, part one-ish?

---
+++ 2017/day-25.rb
@@ -0,0 +1,23 @@
+# Manual input, parsing would be annoying
+input = {
+  'A' => [[1,  1, 'B'], [0, -1, 'F']],
+  'B' => [[0,  1, 'C'], [0,  1, 'D']],
+  'C' => [[1, -1, 'D'], [1,  1, 'E']],
+  'D' => [[0, -1, 'E'], [0, -1, 'D']],
+  'E' => [[0,  1, 'A'], [1,  1, 'C']],
+  'F' => [[1, -1, 'A'], [1,  1, 'A']],
+}
+state = 'A'
+target = 12794428
+
+tape = Hash.new{0}
+ptr = 0
+target.times do
+  val = tape[ptr]
+  op = input[state][val]
+  tape[ptr] = op[0]
+  ptr += op[1]
+  state = op[2]
+end
+
+puts tape.values.count(1)
