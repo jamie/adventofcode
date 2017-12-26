ad24b283330b02e1cc6b0705d975a35be5a9aea1
Jamie Macey <jamie.git@tracefunc.com>
2017-12-25 21:29:22 -0800

    condense this right down to its essence

--- 2017/day-23.rb
+++ 2017/day-23.rb
@@ -20,51 +20,8 @@ prog = MulCountingProgram.new(input)
 prog.run
 puts prog.mul_count
 
-# part 2: Manually convert assembly to ruby, and then hand-optimize
+# part 2: Manually convert assembly to ruby, and then hand-optimize a lot
 
-def run(a)
-  mul = 0
-  h = 0
-  b = 99             # set b 99
-  c = b              # set c b
-  if a != 0          # jnz a 2
-                     # jnz 1 5     ------\
-    b *= 100         # mul b 100         |
-    b += 100_000     # add b 100000      |
-    c = b            # set c b           |
-    c += 17_000      # add c 17000       |
-  end
-  loop do            #             <----<
-    f = 1            # set f 1           |
-    d = 2            # set d 2           |
-    begin
-      e = 2          # set e 2     <---\ |
-      begin
-                     # set g d     <-\ | |
-        mul += 1             # mul g e       | | |
-        g = d*e-b    # sub g b       | | |
-        if g == 0    # jnz g 2  --\  | | |
-          f = 0      # set f 0    |  | | |
-        end
-        e += 1       # add e 1  <-/  | | |
-                     # set g e       | | |
-        g = e-b      # sub g b       | | |
-      end while g != 0 # jnz g -8  --/ | |
-      d += 1         # add d 1         | |
-                     # set g d         | |
-      g = d-b        # sub g b         | |
-    end while g != 0 # jnz g -13   ----/ |
-    if f == 0        # jnz f 2           |
-      h += 1         # add h 1           |
-    end
-                     # set g b           |
-    g = b-c          # sub g c           |
-    if g == 0        # jnz g 2           | -----\
-      puts mul
-      return h       # jnz 1 3           | -\   |
-    end
-    b += 17          # add b 17          |  | <-/
-  end                # jnz 1 -23   ------/  v
-end
-puts run(0)
-puts run(1)
+puts 109_900.step(126_900, 17).select{|b|
+  2.upto(Math.sqrt(b)).any?{|d| b % d == 0}
+}.count
