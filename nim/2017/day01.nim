73fccea0e929dfc5fc3d7636db56443e04f7684e
Jamie Macey <jamie.git@tracefunc.com>
2018-10-09 20:01:20 -0700

    make it more functional

--- nim/2017/day01.nim
+++ nim/2017/day01.nim
@@ -13,6 +13,22 @@ const
   digits = getDigits()
   size = len(digits)
 
+# Functional
+proc count(digits:seq[int], shifted_digits:seq[int]): int =
+  digits.zip(shifted_digits).
+    filter(proc (t:tuple[a:int, b:int]): bool = t.a == t.b ).
+    map(proc (t:tuple[a:int, b:int]): int = t.a ).
+    foldl( a + b )
+
+var digits_shift_1 =
+  digits[1..(len(digits)-1)].concat(@[digits[0]])
+var digits_shift_half =
+  digits[(size/%2)..(len(digits)-1)].concat(digits[0..(size/%2-1)])
+
+echo count(digits, digits_shift_1)
+echo count(digits, digits_shift_half)
+
+# Imperative
 var
   sum: int = 0
   sum2: int = 0
@@ -21,10 +37,10 @@ var
 for i in 0..size-1:
   if digits[i] == digits[(i+1) %% size]:
     sum += digits[i]
-echo sum
+# echo sum
 
 # Part 2
 for i in 0..size-1:
   if digits[i] == digits[(i + size/%2) %% size]:
     sum2 += digits[i]
-echo sum2
+# echo sum2
