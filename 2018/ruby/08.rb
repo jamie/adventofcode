f26eb8cecdc7ba197c3ffb3e8bb6eb26359d634e
Jamie Macey <jamie.git@tracefunc.com>
2018-12-07 21:21:06 -0800

    day 8

---
+++ 2018/ruby/08.rb
@@ -0,0 +1,29 @@
+require 'advent'
+input = Advent.input(2018, 8)[0].split(" ").map(&:to_i)
+
+def sum_metadata(input)
+  sum = 0
+  children = input.shift
+  metacount = input.shift
+  children.times { sum += sum_metadata(input) }
+  metacount.times { sum += input.shift }
+  sum
+end
+
+puts sum_metadata(input.dup)
+
+def value_of(input)
+  sum = 0
+  children = input.shift
+  metacount = input.shift
+  child_values = [nil]
+  children.times { child_values << value_of(input) }
+  if children == 0
+    metacount.times { sum += input.shift }
+  else
+    metacount.times { sum += (child_values[input.shift] || 0) }
+  end
+  sum
+end
+
+puts value_of(input.dup)
