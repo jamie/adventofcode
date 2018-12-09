9b5560ce4633afd19c57c172106be98d49e99e6a
Jamie Macey <jamie.git@tracefunc.com>
2018-12-08 21:28:38 -0800

    day 9, finally something I had to optimize :)

---
+++ 2018/ruby/09.rb
@@ -0,0 +1,85 @@
+require 'advent'
+input = Advent.input(2018, 9)
+
+# 470 players; last marble is worth 72170 points
+
+PLR = 470
+PNT = 72170
+
+# Part 1
+
+players = [0] * PLR
+player = 0
+board = [0]
+current = 0
+
+PNT.times do |i|
+  marble = i + 1
+
+  if marble % 23 == 0
+    players[player] += marble
+    current -= 7
+    current %= board.size
+    players[player] += board.delete_at(current)
+    current %= board.size
+  else
+    current += 2
+    current %= board.size
+    board.insert(current, marble)
+  end
+
+  player += 1
+  player %= players.count
+end
+
+puts players.max
+
+# Part 2
+
+# Had to switch Array out for a linked list, mutating the middle of an Array
+# is really just too slow at that size.
+class Node
+  attr_accessor :value, :left, :right
+  def initialize(value, left=nil, right=nil)
+    @value = value
+    @left = left || self
+    @right = right || self
+  end
+
+  def delete!
+    left.right = right
+    right.left = left
+    [right, value]
+  end
+
+  def append!(value)
+    node = Node.new(value, self, right)
+    self.right = node
+    node.right.left = node
+    node
+  end
+end
+
+players = [0] * PLR
+player = 0
+board = Node.new(0)
+current = board
+
+(PNT*100).times do |i|
+  marble = i + 1
+
+  if marble % 23 == 0
+    players[player] += marble
+    7.times { current = current.left }
+    current, deleted_val = current.delete!
+    players[player] += deleted_val
+  else
+    current = current.right
+    current = current.append!(marble)
+  end
+
+  player += 1
+  player %= players.count
+end
+
+puts players.max
