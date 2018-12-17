b14b946713b63ab75814a54a2cf06240fb85c9d0
Jamie Macey <jamie.git@tracefunc.com>
2018-12-16 22:46:21 -0800

    clean up just a touch

--- 2018/ruby/17.rb
+++ 2018/ruby/17.rb
@@ -1,7 +1,7 @@
 require 'advent'
 input = Advent.input(2018, 17)
 
-grid = (['.' * 2000] * 2000).map(&:dup)
+grid = ([['.'] * 2000] * 2000).map(&:dup)
 
 xmin = ymin = 2000
 xmax = ymax = 0
@@ -24,14 +24,23 @@ input.each do |line|
     end
   end
 end
+# Adjust scan range for stuff that flows outside our bounds
+# but definitely use them to slice for performance
+xmin -= 1
+xmax += 1
 
 
-$grid = grid
-def support?(y, x)
-  %w(# ~).include?($grid[y][x])
+def grid.wall?(y, x)
+  %w(#).include?(self[y][x])
 end
-def empty?(y,x)
-  %w(.).include?($grid[y][x])
+def grid.support?(y, x)
+  %w(# ~).include?(self[y][x])
+end
+def grid.empty?(y, x)
+  %w(.).include?(self[y][x])
+end
+def grid.set(y, x, val)
+  self[y][x] = val
 end
 
 waters = [[0, 500]]
@@ -41,7 +50,7 @@ loop do
   y, x = waters.last
 
   # Record this as water
-  grid[y][x] = '|' if grid[y][x] == '.'
+  grid.set(y, x, '|') if grid.empty?(y, x)
   
   # Terminate
   if y > ymax || grid[y+1][x] == '|'
@@ -49,50 +58,45 @@ loop do
     next
   end
 
-  waters << [y+1, x] if empty?(y+1, x)
+  waters << [y+1, x] if grid.empty?(y+1, x)
 
-  if support?(y+1, x)
+  if grid.support?(y+1, x)
     # Sitting on flat ground, or more water
     # check for solid ground leading to walls, if enclosed fill with water
     solid_left = x.downto(0) do |x2|
-      break x2 if grid[y][x2] == '#'
-      break false unless support?(y+1, x2)
+      break x2 if grid.wall?(y, x2)
+      break false unless grid.support?(y+1, x2)
       false
     end
     solid_right = x.upto(grid[y].size-1) do |x2|
-      break x2 if grid[y][x2] == '#'
-      break false unless support?(y+1, x2)
+      break x2 if grid.wall?(y, x2)
+      break false unless grid.support?(y+1, x2)
       false
     end
     if solid_left && solid_right
       ((solid_left+1)..(solid_right-1)).each do |x2|
-        grid[y][x2] = '~'
+        grid.set(y, x2, '~')
       end
       waters.delete([y, x])
     end
   end
 
   # spread pressure sideways
-  if support?(y+1, x) # supported below
-    waters << [y, x-1] if empty?(y, x-1)
-    waters << [y, x+1] if empty?(y, x+1)
+  if grid.support?(y+1, x) # supported below
+    waters << [y, x-1] if grid.empty?(y, x-1)
+    waters << [y, x+1] if grid.empty?(y, x+1)
     waters.delete([y, x])
   end
 end
 
 # puts grid.map.with_index{|row, i| row[xmin..xmax] + " #{i}"}[(ymin+1)..(ymax-1)]
 
-# Adjust scan range for stuff that flows outside our bounds
-# but definitely use them to slice for performance
-xmin -= 1
-xmax += 1
-
 # Part 1
 puts grid[ymin..ymax].map{|line|
-  line[xmin..xmax].split(//)
+  line[xmin..xmax]
 }.flatten.select{|e| %w(| ~).include? e}.size
 
 # Part 2
 puts grid[ymin..ymax].map{|line|
-  line[xmin..xmax].split(//)
+  line[xmin..xmax]
 }.flatten.select{|e| %w(~).include? e}.size
