332e25a907573337cee04bd9b29b8737d892c61f
Jamie Macey <jamie.git@tracefunc.com>
2018-12-20 23:16:42 -0800

    many small refactorings got us sub-second now

--- 2018/ruby/21.rb
+++ 2018/ruby/21.rb
@@ -71,51 +71,26 @@ end
 # Actually simulating the elfscript is too slow
 # rewrite in ruby (based on the annotated code above)
 
-def run(r, seen)
-  r[2] = r[5] | 65536 # 6
-  r[5] = 7571367      # 7
-  loop do
-    r[4] = r[2] & 255 # 8
-    r[5] += r[4]      # 9
-    r[5] &= 16777215  # 10
-    r[5] *= 65899     # 11
-    r[5] &= 16777215  # 12
-    if 256 > r[2]     # 13
-      exit if (r[5] == r[0]) # 28-29
-      if seen.include?(r[5])
-        puts seen.last
-        exit
-      end
-      seen << r[5]
-      return          # 30
-    end
-
-    r[4] = 0 # 17
-    loop do
-      r[3] = r[4] + 1 # 18
-      r[3] *= 256     # 19
-      if r[3] > r[2]  # 20-21
-        r[2] = r[4]   # 26
-        break
-      end
-      r[4] += 1 # 24
-    end
-  end
-end
-
-r = [0,0,0,0,0,0]
+r5 = 0
 seen = []
 
-# Sanity check
-r[5] = 123
-loop do
-  r[5] &= 456
-  if r[5] == 72
-    break
-  end
-end
-r[5] = 0
+while !seen.include?(r5) do # 30
+  seen << r5
+  r2 = r5 | 65536 # 6
+  r5 = 7571367    # 7
+  r5 = (((r5 + (r2 & 255))) * 65899) & 0xffffff # 8-12, unrolled
+  while 256 <= r2 do # 13
+    # r4 = 0 # 17
+    # r3 = (r4 + 1) * 256 # 18-19, unrolled
+    # while r3 <= r2  # 20-21
+    #   r4 += 1 # 24
+    #   r3 = (r4 + 1) * 256 # 18-19
+    # end
+    # r2 = r4   # 26
+    r2 /= 256 # 17-26, using division
 
-loop do
-  run(r, seen)
+    r5 = (((r5 + (r2 & 255))) * 65899) & 0xffffff # 8-12
+  end
+  # exit if (r5 == r[0]) # 28-29
 end
+puts seen.last
