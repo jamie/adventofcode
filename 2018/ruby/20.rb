3c4d383b5149dc1ca12b01b0b585d74fe1b01ca3
Jamie Macey <jamie.git@tracefunc.com>
2019-01-10 15:39:24 -0800

    day 20, after some attempts at hacking a regex munge

---
+++ 2018/ruby/20.rb
@@ -0,0 +1,96 @@
+require 'advent'
+input = Advent.input(2018, 20)[0]
+
+# Part 1
+# Attempt the third, actually building the maze
+
+class Room < Struct.new(:x, :y, :dist)
+  attr_accessor :east, :west, :north, :south
+
+  def add_room(direction)
+    case direction
+    when 'E'
+      return self.east if self.east
+      room = Room.new(x+1, y, dist+1)
+      self.east = room
+      room.west = self
+    when 'W'
+      return self.west if self.west
+      room = Room.new(x-1, y, dist+1)
+      self.west = room
+      room.east = self
+    when 'N'
+      return self.north if self.north
+      room = Room.new(x, y-1, dist+1)
+      self.north = room
+      room.south = self
+    when 'S'
+      return self.south if self.south
+      room = Room.new(x, y+1, dist+1)
+      self.south = room
+      room.north = self
+    else
+      fail "Unknown direction: #{direction}"
+    end
+    room
+  end
+
+  def to_s
+    return '╋' if x.zero? && y.zero?
+
+    case [!!east, !!west, !!north, !!south]
+    when [false, false, false, false]; fail "Empty Room!"
+    when [false, false, false, true ]; "╷" # south
+    when [false, false, true , false]; "╵" # north
+    when [false, false, true , true ]; "│"
+    when [false, true , false, false]; "╴" # west
+    when [false, true , false, true ]; "┐"
+    when [false, true , true , false]; "┘"
+    when [false, true , true , true ]; "┤"
+    when [true , false, false, false]; "╶" # east
+    when [true , false, false, true ]; "┌"
+    when [true , false, true , false]; "└"
+    when [true , false, true , true ]; "├"
+    when [true , true , false, false]; "─"
+    when [true , true , false, true ]; "┬"
+    when [true , true , true , false]; "┴"
+    when [true , true , true , true ]; "┼"
+    end
+  end
+end
+
+home = Room.new(0, 0, 0)
+map = {[0,0] => home}
+stack = [home]
+input.split(//).each do |char|
+  case char
+  when '^', '$'
+    next
+  when /[NEWS]/
+    room = stack[-1].add_room(char)
+    map[[room.x, room.y]] = room
+    stack[-1] = room
+  when '('
+    stack << stack[-1]
+  when '|'
+    stack.pop
+    stack << stack[-1]
+  when ')'
+    stack.pop
+  end
+end
+
+xs = map.keys.map(&:first).uniq.sort
+ys = map.keys.map(&:last).uniq.sort
+
+# ys.each do |y|
+#   xs.each do |x|
+#     print map[[x, y]].to_s
+#   end
+#   puts
+# end
+
+puts map.values.map(&:dist).max
+
+# Part 2, counting
+puts map.values.map(&:dist).count{|e| e >= 1000 }
