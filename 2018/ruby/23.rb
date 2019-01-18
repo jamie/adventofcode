b40e5e2239e40623a19ca8d4d76806647dd1e9b2
Jamie Macey <jamie.git@tracefunc.com>
2019-01-17 21:09:17 -0800

    day 23 part 2, subdivision search is quick when it works

--- 2018/ruby/23.rb
+++ 2018/ruby/23.rb
@@ -2,6 +2,9 @@ require 'advent'
 input = Advent.input(2018, 23)
 
 class Point < Struct.new(:x, :y, :z)
+  def distance_to(other)
+    [x-other.x, y-other.y, z-other.z].map(&:abs).sum
+  end
 end
 
 class Nanobot < Struct.new(:x, :y, :z, :r)
@@ -23,3 +26,126 @@ strongest = bots.sort_by(&:r).last
 puts bots.select{|bot| strongest.in_range?(bot) }.size
 
 # Part 2
+class PStack
+  attr_reader :queue
+  def initialize
+    @queue = {}
+  end
+
+  def add(priority, value)
+    @queue[priority] ||= []
+    @queue[priority] << value
+  end
+
+  def pop
+    priority = @queue.keys.max
+    value = @queue[priority].pop
+    if @queue[priority].empty?
+      @queue.delete(priority)
+    end
+    value
+  end
+end
+
+class Region < Struct.new(:xmin, :xmax, :ymin, :ymax, :zmin, :zmax)
+  def coverage(nanobots)
+    nanobots.select { |bot| reachable?(bot) }.size
+  end
+
+  def reachable?(bot)
+    distance = 0
+    if bot.x < xmin
+      distance += (xmin-bot.x)
+    elsif bot.x > xmax
+      distance += (bot.x-xmax)
+    end
+    if bot.y < ymin
+      distance += (ymin-bot.y)
+    elsif bot.y > ymax
+      distance += (bot.y-ymax)
+    end
+    if bot.z < zmin
+      distance += (zmin-bot.z)
+    elsif bot.z > zmax
+      distance += (bot.z-zmax)
+    end
+    distance <= bot.r
+  end
+
+  def point?
+    xmin == xmax ||
+    ymin == ymax ||
+    zmin == zmax
+  end
+
+  def invalid?
+    xmin > xmax ||
+    ymin > ymax ||
+    zmin > zmax
+  end
+
+  def subdivisions
+    # eight of 'em
+    xmid = (xmin + xmax) / 2
+    ymid = (ymin + ymax) / 2
+    zmid = (zmin + zmax) / 2
+    [ Region.new(xmin,   xmid, ymin,   ymid, zmin,   zmid),
+      Region.new(xmid+1, xmax, ymin,   ymid, zmin,   zmid),
+      Region.new(xmin,   xmid, ymid+1, ymax, zmin,   zmid),
+      Region.new(xmid+1, xmax, ymid+1, ymax, zmin,   zmid),
+      Region.new(xmin,   xmid, ymin,   ymid, zmid+1, zmax),
+      Region.new(xmid+1, xmax, ymin,   ymid, zmid+1, zmax),
+      Region.new(xmin,   xmid, ymid+1, ymax, zmid+1, zmax),
+      Region.new(xmid+1, xmax, ymid+1, ymax, zmid+1, zmax),
+    ].reject(&:invalid?)
+  end
+
+  def volume
+    ((xmax-xmin+1) * (ymax-ymin+1) * (zmax-zmin+1))
+  end
+  
+  def size
+    volume.to_s.size
+  end
+
+  def midpoint
+    Point.new(
+      (xmin + xmax) / 2,
+      (ymin + ymax) / 2,
+      (zmin + zmax) / 2,
+    )
+  end
+end
+
+xs = bots.map(&:x)
+ys = bots.map(&:y)
+zs = bots.map(&:z)
+universe = Region.new(xs.min, xs.max, ys.min, ys.max, zs.min, zs.max)
+
+search = PStack.new
+search.add(universe.coverage(bots), universe)
+
+n = 0
+findings = []
+coverage = nil
+loop do
+  region = search.pop
+
+  if region.volume == 1
+    cov = region.coverage(bots)
+    coverage ||= cov
+    if cov < coverage
+      break
+    elsif cov == coverage
+      findings << region
+    end
+    next
+  end
+
+  region.subdivisions.each do |subregion|
+    search.add(subregion.coverage(bots), subregion)
+  end
+end
+
+origin = Point.new(0, 0, 0)
+pp findings.sort_by{|region| region.midpoint.distance_to(origin)}.first.midpoint.distance_to(origin)
