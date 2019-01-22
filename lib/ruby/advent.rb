63fd9ea616bc51a4f6b45d79ec24b77eedcb9497
Jamie Macey <jamie.git@tracefunc.com>
2019-01-22 11:21:43 -0800

    cleanup

--- lib/ruby/advent.rb
+++ lib/ruby/advent.rb
@@ -4,7 +4,7 @@ require 'date'
 
 module Advent
   def self.input(year, day, format=:to_s)
-    File.read('%4d/input/%02d' % [year, day]).chomp.split("\n").map(&format)
+    File.readlines('%4d/input/%02d' % [year, day]).map(&:chomp).map(&format)
   end
 end
 
