4c5d8b1df7acfbf5023b9affef747f79615ebd05
Jamie Macey <jamie.git@tracefunc.com>
2019-01-22 11:41:34 -0800

    giant refactor/cleanup here

--- 2015/ruby/14.rb
+++ 2015/ruby/14.rb
@@ -1,62 +1,36 @@
 require 'advent'
 input = Advent.input(2015, 14)
 
+TIME = 2503
+
+Reindeer = Struct.new(:name, :speed, :endurance, :recovery, :location, :points) do
+  attr_accessor :activity
+  def initialize(*)
+    super
+    self.activity = [:fly] * endurance + [:rest] * recovery
+  end
+
+  def travel!
+    self.location += speed if activity[0] == :fly
+    activity << activity.shift
+  end
+
+  def score!
+    self.points += 1
+  end
+end
+
+reindeer = input.map do |line|
+  line =~ /(.*) can fly (.*) km\/s for (.*) seconds, but then must rest for (.*) seconds./
+  Reindeer.new($1, $2.to_i, $3.to_i, $4.to_i, 0, 0)
+end
+
+TIME.times do
+  reindeer.each(&:travel!)
+  reindeer.sort_by(&:location).last.score!
+end
+
 # Part 1
-reindeer = []
-race_time = 2503
-
-input.each do |line|
-  line =~ /(.*) can fly (.*) km\/s for (.*) seconds, but then must rest for (.*) seconds./
-  reindeer << Regexp.last_match.captures.map{|val| val =~ /\d/ ? val.to_i : val}
-end
-
-reindeer.each do |deer|
-  name, speed, endurance, recovery = deer
-
-  distance = 0
-  time = 0
-  while (time+endurance) < race_time do
-    time += endurance
-    distance += speed * endurance
-    time += recovery
-  end
-  while time < race_time do
-    time += 1
-    distance += speed
-  end
-
-  deer << distance
-end
-
-puts reindeer.sort_by(&:last).last.last
-
+puts reindeer.map(&:location).max
 # Part 2
-reindeer = []
-race_time = 2503
-
-input.each do |line|
-  line =~ /(.*) can fly (.*) km\/s for (.*) seconds, but then must rest for (.*) seconds./
-  reindeer << {
-    name:      $1,
-    speed:     $2.to_i,
-    endurance: $3.to_i,
-    recovery:  $4.to_i,
-    location:  0,
-    points:    0,
-  }
-end
-
-reindeer.each do |deer|
-  deer[:activity] = [:fly] * deer[:endurance] + [:rest] * deer[:recovery]
-end
-
-race_time.times do
-  reindeer.each do |deer|
-    action = deer[:activity].shift
-    deer[:location] += deer[:speed] if action == :fly
-    deer[:activity] << action
-  end
-  reindeer.sort_by{|deer| deer[:location]}.last[:points] += 1
-end
-
-puts reindeer.map{|deer| deer[:points]}.sort.last
+puts reindeer.map(&:points).max
