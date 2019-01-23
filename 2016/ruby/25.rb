87b3ddb949726cc630d31ccd40dc2b43f0c933ab
Jamie Macey <jamie.git@tracefunc.com>
2019-01-23 10:28:34 -0800

    extract assembunny CPU

--- 2016/ruby/25.rb
+++ 2016/ruby/25.rb
@@ -1,63 +1,7 @@
 require 'advent'
 input = Advent.input(2016, 25)
 
-class Assembunny
-  attr_accessor :prog, :output, :registers
-
-  def initialize(prog)
-    @prog = prog
-    reset!
-  end
-
-  def reset!
-    @output = []
-    @registers = Hash.new(0)
-  end
-
-  def set_register(key, value)
-    registers[key] = value
-  end
-
-  def run
-    pc = 0
-    while prog[pc]
-      op = prog[pc]
-      pc += 1
-      case op
-      when /cpy (.*) (.*)/
-        src = $1
-        dst = $2
-        if src =~ /\d/
-          src = src.to_i
-        else
-          src = registers[src]
-        end
-        registers[dst] = src
-      when /inc (.*)/
-        registers[$1] += 1
-      when /dec (.*)/
-        registers[$1] -= 1
-      when /jnz (.*) (.*)/
-        src = $1
-        mov = $2.to_i
-        if src =~ /\d/
-          src = src.to_i
-        else
-          src = registers[src]
-        end
-        if src != 0
-          pc += mov - 1
-        end
-      when /out (.*)/
-        output << registers[$1]
-        return if output.size > 10
-      else
-        puts "Unknown op: #{op}"
-        exit
-      end
-    end
-  end
-end
+require 'assembunny'
 
 cpu = Assembunny.new(input)
 10_000.times do |i|
---
+++ 2016/ruby/assembunny.rb
@@ -0,0 +1,69 @@
+class Assembunny
+  attr_accessor :prog, :output, :registers
+
+  def initialize(prog)
+    @prog = prog
+    reset!
+  end
+
+  def reset!
+    @output = []
+    @registers = Hash.new(0)
+  end
+
+  def set_register(key, value)
+    registers[key] = value
+  end
+
+  def run
+    pc = 0
+    while prog[pc]
+      op = prog[pc]
+      pc += 1
+      case op
+      when /cpy (.*) (.*)/
+        src = $1
+        dst = $2
+        if src =~ /\d/
+          src = src.to_i
+        else
+          src = registers[src]
+        end
+        registers[dst] = src
+      when /inc (.*)/
+        registers[$1] += 1
+      when /dec (.*)/
+        registers[$1] -= 1
+      when /jnz (.*) (.*)/
+        src = $1
+        mov = $2.to_i
+        if src =~ /\d/
+          src = src.to_i
+        else
+          src = registers[src]
+        end
+        if src != 0
+          pc += mov - 1
+        end
+      when /tgl (.*)/
+        index = registers[$1] + pc
+        next if prog[index].nil?
+        puts "Toggle #{index+pc} of #{prog.size}"
+        prog[index] = case prog[index]
+        when /(.*) (.*) (.*)/
+          "#{($1 == "jnz") ? "cpy" : "jnz"} #{$2} #{$3}"
+        when /(.*) (.*)/
+          "#{($1 == "inc") ? "dec" : "inc"} #{$2}"
+        end
+        require 'pp'
+        pp prog
+      when /out (.*)/
+        output << registers[$1]
+        return if output.size > 10
+      else
+        puts "Unknown op: #{op}"
+        exit
+      end
+    end
+  end
+end
