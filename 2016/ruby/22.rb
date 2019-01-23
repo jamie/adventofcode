332826f3ba46eef60e1ac1d9c193b47c888dec55
Jamie Macey <jamie.git@tracefunc.com>
2019-01-23 10:24:37 -0800

    second star

--- 2016/ruby/22.rb
+++ 2016/ruby/22.rb
@@ -24,42 +24,43 @@ puts viable.uniq.size
 # Node, Size, Used, Avail, Use%
 dfh = input.map{|line| line.chomp.split(/T? +/)}
 
+Cell = Struct.new(:used, :size) do
+  def empty?
+    used.zero?
+  end
+end
+
 map = []
 dfh.each do |node, size, used, avail, _|
   node =~ /x(\d+)-y(\d+)/
   x, y = $1.to_i, $2.to_i
   map[y] ||= []
-  map[y][x] = [used.to_i, size.to_i]
+  map[y][x] = Cell.new(used.to_i, size.to_i)
 end
 
-threshold = 70
-
-map.each do |row|
-  row.each do |cell|
-    if cell[0] > threshold
-      # data very large
-      print '%'
-    elsif cell[1] < threshold
-      # size too small for target
-      print '#'
-    elsif cell[0].zero?
-      print '_'
-    else
-      print '.'
-    end
-  end
-  puts
-end
-
-# viable = []
-#
-# dfh.each do |node_a, size_a, used_a, avail_a, _|
-#   dfh.each do |node_b, size_b, used_b, avail_b, _|
-#     next if used_a == '0'
-#     next if node_a == node_b
-#     next if used_a.to_i > avail_b.to_i
-#     viable << [node_a, node_b].sort
+# threshold = map[0].last.used
+# map.each do |row|
+#   row.each do |cell|
+#     if cell.empty?
+#       print '_'
+#     elsif cell.used > 100
+#       # data very large
+#       print '%'
+#     elsif cell.size < threshold
+#       # size too small for target
+#       print '#'
+#     else
+#       print '.'
+#     end
 #   end
+#   puts
 # end
-#
-# puts viable.uniq.size
+
+# By inspection of map (above) there is one empty node.
+# It takes 64 steps to move that empty space to the top
+# right corner (moving the desired data one space left).
+# It then takes 5 steps to loop around the data and move
+# it an additional space left. The grid is 39 spaces
+# wide, so data needs to move left 38 times, and we've
+# already moved it once so we need to do 37 cycles.
+puts 64 + (5 * 37)
