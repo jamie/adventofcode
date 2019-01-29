d55b15e883c88f71095cb4e3130b3744074e3c4c
Jamie Macey <jamie.git@tracefunc.com>
2019-01-29 15:17:45 -0800

    slight performance boost reducing duplicate work. still slow

--- 2016/ruby/05.rb
+++ 2016/ruby/05.rb
@@ -1,32 +1,24 @@
 require 'advent'
 input = Advent.input(2016, 5)
 
-# Part 1
+# Parts 1 and 2 require ~1m and 2.5m iterations,
+# calculations merged to avoid a bunch of duplicate work
 password = ""
+password2 = []
+password2_chars = 0
 
 i = 0
-while password.size < 8
+while password.size < 8 || password2_chars < 8
   digest = Digest::MD5.new.hexdigest(input + i.to_s)
   if digest =~ /^00000(.)/
   	password << $1
   end
-  i += 1
-end
-
-puts password
-
-# Part 2
-password = "________"
-
-i = 0
-c = 0
-while c < 8
-  digest = Digest::MD5.new.hexdigest(input + i.to_s)
-  if digest =~ /^00000([0-7])(.)/ && password[$1.to_i] == '_'
-  	password[$1.to_i] = $2
-  	c += 1
+  if digest =~ /^00000([0-7])(.)/ && password2[$1.to_i].nil?
+    password2[$1.to_i] = $2
+    password2_chars += 1
   end
   i += 1
 end
 
 puts password
+puts password2.join
