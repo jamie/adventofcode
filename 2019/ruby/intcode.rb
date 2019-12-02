16d39d7cead9d0fa330a3242044833380903898a
Jamie Macey <jamie.git@tracefunc.com>
2019-12-01 21:30:18 -0800

    extract to lib

---
+++ 2019/ruby/intcode.rb
@@ -0,0 +1,55 @@
+class Intcode
+  attr_reader :ip, :memory
+
+  def initialize(input)
+    @memory_base = input.split(",").map(&:to_i)
+  end
+
+  def reset(noun, verb)
+    @memory = @memory_base.dup
+    memory[1] = noun
+    memory[2] = verb
+  end
+
+  def execute(noun, verb)
+    reset(noun, verb)
+
+    @ip = 0
+    loop do
+      case memory[ip]
+      when 1
+        add
+      when 2
+        mul
+      else
+        break
+      end
+    end
+    memory[0]
+  end
+
+  private
+
+  def args(*kind)
+    kind.map.with_index do |k, i|
+      case k
+      when :p # pointer
+        memory[ip + i + 1]
+      else
+        fail "Unknown argument kind: #{k}"
+      end
+    end
+  end
+
+  def add
+    p1, p2, p3 = args(:p, :p, :p)
+    memory[p3] = memory[p1] + memory[p2]
+    @ip += 4
+  end
+
+  def mul
+    p1, p2, p3 = args(:p, :p, :p)
+    memory[p3] = memory[p1] * memory[p2]
+    @ip += 4
+  end
+end
