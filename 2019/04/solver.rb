d61b4f529adf1146e571bf98c64efe45530ad241
Jamie Macey <jamie.git@tracefunc.com>
2019-12-03 21:46:07 -0800

    add a comment

--- 2019/04/solver.rb
+++ 2019/04/solver.rb
@@ -8,6 +8,7 @@ min.upto(max).each do |password|
   password = password.to_s
 
   next unless password =~ /(\d)\1/
+  #                       v # lookahead regex to match overlaps
   next if password.scan(/(?=(..))/).any? { |x| x[0][0] > x[0][1] }
   count += 1
   count2 += 1 if password.scan(/((\d)\2+)/).any? { |group| group[0].size == 2 }
