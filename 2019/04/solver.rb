b7dedba3afaa1ac1f4432747fc165d2011e79729
Jamie Macey <jamie.git@tracefunc.com>
2019-12-03 21:42:54 -0800

    optimize performance to sub-second, with more regexes!

--- 2019/04/solver.rb
+++ 2019/04/solver.rb
@@ -3,31 +3,15 @@ input = Advent.input(2019, 4)
 
 min, max = input.split("-").map(&:to_i)
 
-count = 0
-min.upto(max).each do |password|
-  next unless password.to_s =~ /(\d)\1/
-  digits = password.to_s.split(//).map(&:to_i)
-  next unless digits == digits.sort
-  count += 1
-end
-
-puts count
-
-count = 0
+count = count2 = 0
 min.upto(max).each do |password|
   password = password.to_s
 
   next unless password =~ /(\d)\1/
-  digits = password.split(//).map(&:to_i)
-  next unless digits == digits.sort
-
-  short = false
-  password.scan(/(\d)\1/).each do |group|
-    digit = group[0]
-    short = true if (password =~ /#{digit}{3}/).nil?
-  end
-
-  count += 1 if short
+  next if password.scan(/(?=(..))/).any? { |x| x[0][0] > x[0][1] }
+  count += 1
+  count2 += 1 if password.scan(/((\d)\2+)/).any? { |group| group[0].size == 2 }
 end
 
 puts count
+puts count2
