e04f102a25f0a6d8429ab9975cbc8fadb2732522
Jamie Macey <jamie.git@tracefunc.com>
2019-12-06 22:16:50 -0800

    fix old solutions to work with new intcode I/O

--- 2019/02/solver.rb
+++ 2019/02/solver.rb
@@ -5,13 +5,14 @@ require "intcode"
 intcode = Intcode.new(input)
 
 # Part 1
-puts intcode.reset(12, 2).execute
+intcode.reset(12, 2).execute
+puts intcode.memory[0]
 
 # Part 2
 (0..99).each do |noun|
   (0..99).each do |verb|
-    value = intcode.reset(noun, verb).execute
-    if value == 19690720
+    intcode.reset(noun, verb).execute
+    if intcode.memory[0] == 19690720
       puts noun * 100 + verb
       exit
     end
--- 2019/05/solver.rb
+++ 2019/05/solver.rb
@@ -5,5 +5,9 @@ require 'intcode'
 
 intcode = Intcode.new(input)
 
-intcode.reset.execute([1])
-intcode.reset.execute([5])
+intcode.reset
+out = intcode.execute([1])
+out = intcode.execute while out == 0
+puts out
+
+puts intcode.reset.execute([5])
