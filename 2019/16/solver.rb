3260b751d2f8cddf5a07a8e30bf79714034eb19f
Jamie Macey <jamie.git@tracefunc.com>
2019-12-16 23:06:49 -0800

    day 16 part 2, after some internet hints

--- 2019/16/solver.rb
+++ 2019/16/solver.rb
@@ -1,18 +1,19 @@
 require "advent"
 input = Advent.input.split(//).map(&:to_i)
 
-# Part 1
-signal = input
-
 PATTERN = [0, 1, 0, -1].freeze
 
-def pattern(i, len)
+def stretch(i, len)
   pat = rep = PATTERN.map { |v| [v] * i }.flatten
   pat += rep while pat.size < len + 1
   pat[1...(len + 1)]
 end
 
-patterns = 1.upto(signal.size).map{ |i| pattern(i, signal.size) }
+# Part 1
+
+signal = input
+
+patterns = 1.upto(signal.size).map { |i| stretch(i, signal.size) }
 
 100.times do |c|
   signal = signal.map.with_index do |n, i|
@@ -21,17 +22,28 @@ patterns = 1.upto(signal.size).map{ |i| pattern(i, signal.size) }
 end
 puts signal[0...8].join
 
-# # Part 2
-# input = '03036732577212944063491565474664'.split(//).map(&:to_i)
-# signal = input * 10_000
+# Part 2
 
-# patterns = 1.upto(signal.size).map{|i| pattern(i, signal.size)}
+# Insight from Reddit, offset is over halfway through the puzzle
+# so the transform matrix here is upper triangular:
+# [ 1 1 1 ... ]
+# [ 0 1 1 ... ]
+# [ 0 0 1 ... ]
+# [ 0 0 0 ... ] ...
+# So it's just a sequence of simple adds and muls, from offset onward.
 
-# 100.times do |c|
-#   signal = signal.map.with_index do |n, i|
-#     [signal, patterns[i]].transpose.map { |x, y| x * y }.inject(&:+).abs % 10
-#   end
-# end
+# My own insight was working a running total for speed. It's still slow tho.
 
-# offset = input[0...7].join.map(&:to_i)
-# puts signal[offset...(offset+8)].join
+offset = input[0...7].join.to_i
+
+signal = input * 10_000
+signal = signal[offset..-1] # the only relevant part
+
+100.times do |c|
+  sum = signal.inject(&:+)
+  signal = signal.map do |v|
+    (sum % 10).tap { sum -= v }
+  end
+end
+
+puts signal[0...8].join
