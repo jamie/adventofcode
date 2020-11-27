30d5c6e603e1e17cd35469b0366ee5603899ab47
Jamie Macey <jamie.git@tracefunc.com>
2020-11-27 09:19:51 -0800

    thoughts on some faster code

--- 2019/18/solver.rb
+++ 2019/18/solver.rb
@@ -3,7 +3,7 @@ input = Advent.input
 
 require "priority_deque"
 
-inpxut = <<STR.split("\n")
+input = <<STR.split("\n") # 138
 #################
 #i.G..c...e..H.p#
 ########.########
@@ -15,7 +15,7 @@ inpxut = <<STR.split("\n")
 #################
 STR
 
-xinput = <<STR.split("\n")
+input = <<STR.split("\n") # 8
 #########
 #b.A.@.a#
 #########
@@ -91,5 +91,157 @@ loop do
     new_steps = steps + dist
     new_keys = (keys_held + [target]).sort
     queue.add(new_steps, [new_steps, dest, new_keys])
+  end
+end
+
+__END__
+
+# Some WIP thoughts for solving part 2
+
+
+class ManhattanMap
+  WALL = "#"
+  PATH = "."
+
+  def self.parse(input, wall_sym: WALL, path_sym: PATH)
+    map = {}
+    input.each_with_index do |row, y|
+      row.split(//).each_with_index do |cell, x|
+        next if cell == wall_sym # Skip walls, uninteresting
+        map[[x, y]] = cell
+      end
+    end
+    self.new(map, path_sym: path_sym)
+  end
+
+  def initialize(map, path_sym: PATH)
+    @map = map
+    @path_sym = path_sym
+  end
+
+  def as_distance_graph
+    out = {}
+    (@map.values.uniq - [@path_sym]).sort.each do |poi|
+      out[poi] ||= {}
+
+      cell = @map.key(poi)
+      queue = PriorityDeque.new(:min)
+      queue.add(0, [0, cell])
+      seen = []
+      loop do
+        break if queue.empty?
+        dist, cell = queue.shift
+        dest = @map[cell]
+        next unless dest
+        seen << cell
+        if dest == @path_sym || dest == poi
+          # Queue neighbours
+          x, y = cell
+          dist += 1
+          queue.add(dist, [dist, [x + 1, y]]) unless seen.include?([x + 1, y])
+          queue.add(dist, [dist, [x - 1, y]]) unless seen.include?([x - 1, y])
+          queue.add(dist, [dist, [x, y + 1]]) unless seen.include?([x, y + 1])
+          queue.add(dist, [dist, [x, y - 1]]) unless seen.include?([x, y - 1])
+        else
+          out[poi][dest] ||= dist
+        end
+      end
+    end
+    out
+  end
+
+  def as_dot
+    edges = []
+    as_distance_graph.each do |src, maps|
+      maps.each do |dst, distance|
+        next if src < dst
+        edges << "#{src} -- #{dst} [label=#{distance}]".gsub("@", "Start")
+      end
+    end
+    "graph manhattan {\n#{edges.join("\n")}\n}\n"
+  end
+
+  def dijkstra(start, destinations, graph)
+    q = []
+    dist = {}
+    prev = {}
+    graph.keys.each do |v|
+      dist[v] = 1_000_000
+      prev[v] = nil
+      q << v
+    end
+    dist[start] = 0
+
+    while !q.empty?
+      u = q.sort_by { |qq| dist[qq] }.first
+      q.delete(u)
+      graph[u].each do |v, len|
+        alt = dist[u] + len
+        if alt < dist[v]
+          dist[v] = alt
+          prev[v] = [prev[u], u].compact.join
+        end
+      end
+    end
+    [dist, prev]
+  end
+
+  def as_full_distance_graph(pois)
+    partial = as_distance_graph
+    matrix = {}
+    pois.each do |start|
+      next unless partial[start]
+      dist, path = dijkstra(start, pois, partial)
+      dist.each do |v, steps|
+        matrix[[start, v]] = [steps, path[v]]
+      end
+    end
+    matrix
+  end
+end
+
+distances = ManhattanMap.parse(input).as_full_distance_graph(['@'] + ('a'..'z').to_a)
+distances.delete_if do |k, _v|
+  src, dst = k
+  (
+    src == dst || # No-op
+    dst =~ /[A-Z]/  # Travel to door
+  )
+end
+pp distances
+
+exit
+
+keys = distances.keys.select{ |k| k =~ /[a-z]/ }.sort
+doors = distances.keys.select{ |k| k =~ /[A-Z]/ }.sort
+
+queue = PriorityDeque.new
+queue.add(0, [0, '@', []])
+seen = []
+loop do
+  steps, pos, keys_held = queue.shift
+  p [:pathfind, steps, pos, keys_held.size, seen.size, queue.size]
+
+  next if seen.include?([pos, keys_held])
+  seen << [pos, keys_held]
+
+  if keys_held.sort == keys
+    puts steps
+    break
+  end
+
+  distances[pos].each do |target, distance|
+    if target =~ /[A-Z]/
+      # Skip locked door
+      next unless keys_held.include?(target.downcase)
+    end
+    if target =~ /[a-z]/
+      # Pick up new key
+      new_keys = (keys_held + [target]).sort.uniq
+    else
+      new_keys = keys_held
+    end
+    new_steps = steps + distance
+    queue.add(new_keys.size-new_steps, [new_steps, target, new_keys])
   end
 end
