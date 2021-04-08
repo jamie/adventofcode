87f4caa918e3fd1314c2cf66eeefe2aa3514e0b5
Jamie Macey <jamie.git@tracefunc.com>
2021-04-07 22:30:46 -0700

    ex: 2020 day 2

---
+++ 2020/02/solver.exs
@@ -0,0 +1,38 @@
+input =
+  Advent.read_lines("2020/02/input")
+  |> Enum.map(fn line ->
+    Regex.run(~r/(\d+)-(\d+) (.*): (.*)/, line, capture: :all_but_first)
+  end)
+
+# Part 1
+input
+|> Enum.filter(fn [smin, smax, char, password] ->
+  min = String.to_integer(smin)
+  max = String.to_integer(smax)
+
+  freq =
+    password
+    |> String.graphemes()
+    |> Enum.count(fn c -> c == char end)
+
+  min <= freq && freq <= max
+end)
+|> Enum.count()
+|> IO.puts()
+
+# Part 2
+input
+|> Enum.filter(fn [slo, shi, char, password] ->
+  lo = String.to_integer(slo) - 1
+  hi = String.to_integer(shi) - 1
+
+  chars = password |> String.graphemes()
+
+  matches =
+    [Enum.at(chars, lo), Enum.at(chars, hi)]
+    |> Enum.count(fn c -> c == char end)
+
+  matches == 1
+end)
+|> Enum.count()
+|> IO.puts()
