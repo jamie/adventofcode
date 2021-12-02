f3e9ecfed0bd66632350da4e88f55150a9a9381a
Jamie Macey <jamie.git@tracefunc.com>
2021-12-01 22:19:35 -0800

    rb: 2021-02

--- 2021/02/solver.rb
+++ 2021/02/solver.rb
@@ -1,24 +1,33 @@
 require "advent"
 input = Advent.input
 
-xinput = <<STR.split("\n")
-199
-200
-208
-210
-200
-207
-240
-269
-260
-263
-STR
-
-input.map!(&:to_i)
-
 # Part 1
 
+x, y = 0, 0
+input.each do |line|
+  case line
+  when /forward (\d+)/
+    x += $1.to_i
+  when /down (\d+)/
+    y += $1.to_i
+  when /up (\d+)/
+    y -= $1.to_i
+  end
+end
+puts x * y
 
 # Part 2
 
-
+x, y, aim = 0, 0, 0
+input.each do |line|
+  case line
+  when /forward (\d+)/
+    x += $1.to_i
+    y += $1.to_i * aim
+  when /down (\d+)/
+    aim += $1.to_i
+  when /up (\d+)/
+    aim -= $1.to_i
+  end
+end
+puts x * y
