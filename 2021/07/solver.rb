deb5e8fc274b75e69812e99a1e43a2b94e0767b2
Jamie Macey <jamie@tracefunc.com>
2021-12-07 05:22:12 +0000

    rb: 2021 day 7

---
+++ 2021/07/solver.rb
@@ -0,0 +1,28 @@
+require "advent"
+input = Advent.input
+
+input = "16,1,2,0,4,2,7,1,2,14"
+crabs = input.split(',').map(&:to_i)
+
+# Part 1
+
+cost = Float::INFINITY
+
+(0..(crabs.max)).each do |n|
+  fuel = crabs.map{|crab| (crab-n).abs }.sum
+  cost = fuel if fuel < cost
+end
+
+puts cost
+
+# Part 2
+
+cost = Float::INFINITY
+
+(0..(crabs.max)).each do |n|
+  fuel = crabs.map{|crab| steps = (crab-n).abs; 1.upto(steps).sum }.sum
+  puts fuel
+  cost = fuel if fuel < cost
+end
+
+puts cost
