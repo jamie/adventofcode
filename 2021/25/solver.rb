95c1cec3c82236c76882c7d483b5fe35a59ba431
Jamie Macey <jamie.git@tracefunc.com>
2021-12-29 10:56:57 -0800

    rb: 2021 day 25

---
+++ 2021/25/solver.rb
@@ -0,0 +1,56 @@
+require "advent"
+input = Advent.input
+
+inpxut = <<~STR.split("\n")
+  v...>>.vv>
+  .vv>>.vv..
+  >>.>v>...v
+  >>v>>.>.v.
+  v>v.vv.v..
+  >.>>..v...
+  .vv..>.>v.
+  v.v..>>v.v
+  ....v..v.>
+STR
+
+# Part 1
+
+def step(line, mover)
+  empty = '.'
+  line.map.with_index do |cell, i|
+    if cell == mover
+      right = line[(i+1) % line.size]
+      right == empty ? right : cell
+    elsif cell == empty
+      left = line[(i-1) % line.size]
+      left == mover ? left : cell
+    else
+      cell
+    end
+  end
+end
+
+seabed = input.map{|line| line.split(//)}
+step = 0
+loop do
+  step += 1
+  changed = false
+
+  seabed = seabed.map do |line|
+    line_ = step(line, '>')
+    changed = true unless line == line_
+    line_
+  end
+  seabed = seabed.transpose
+
+  seabed = seabed.map do |line|
+    line_ = step(line, 'v')
+    changed = true unless line == line_
+    line_
+  end
+  seabed = seabed.transpose
+
+  break unless changed
+end
+
+puts step
