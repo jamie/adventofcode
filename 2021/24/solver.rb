530b53f5421fc28e659cf111f2f957de6f40014f
Jamie Macey <jamie.git@tracefunc.com>
2021-12-29 22:31:34 -0800

    rb: 2021 day 24, manual solve

--- 2021/24/solver.rb
+++ 2021/24/solver.rb
@@ -1,7 +1,10 @@
 require "advent"
 input = Advent.input
 
-# Translate input by hand to ruby, then hand-optimize
+# This started with some manual analysis, a bunch of head-scratching,
+# and then 2 minutes of pencil work. In short, we can translate to ruby,
+# extract the duplication to a method, and then hand optimize.
+
 def monad(digits)
   z = 0
   z = monad_digit(z, digits.shift, 1, 10, 2)
@@ -23,33 +26,36 @@ end
 
 def monad_digit(z, w, q, n1, n2)
   if w == z % 26 + n1
-    (z / q)
+    z / q
   else
     (z / q) * 26 + w + n2
   end
 end
 
-# Part 1
+# Then analysis leads to noticing that when q is 1 we prefer the second
+# path, which:
+# - shifts current z state up (* 26)
+# - store w + n2
+# On a 26 however, we note n1 is negative and so the if could be rewritten
+# as `w - n1 == z % 26`. With the insight that we can treat a 1/26 as a pair,
+# then `z % 26` was the `w + n2` from prior iteration.
+# If we can constrain the two relevant input digits to balance some equations,
+# - [1] w + n2
+# - [26] w - n1 (note n1 is negative)
+# then we can simply manually assemble the relevant digit pairings. For my
+# input, these look like:
+#   / - - - - - -7- - - - - - \
+#   | / - -6- - \ / - -8- - \ |
+#   | | /0\ /2\ | | / -7- \ | |
+#   | | | | | | | | | /3\ | | |
+#   9 3 9 9 7 9 9 9 2 9 6 9 1 2
+# giving the highest value (Part 1).
+# Scaling each pair down to include a 1 (ie, making the outer pair 8...1)
+# gives the lowest matching value (Part 2).
 
-n = 0
-n_ = 0
-sum = 0
-min = 11111111111111
-max = 99_999_999_999_999
-max.downto(min) do |i|
-  digits = i.to_s.split(//).map(&:to_i)
-  n_ += 1
-  next if digits.include?(0)
-  n += 1
+puts 93997999296912
+puts 81111379141811
 
-  m = monad(digits)
-  if m == 0
-    puts digits.join
-    break
-  end
-
-  sum += m
-  break if n >= 50_000
-end
-
-p [n, n_, sum]
+# TBD sometime maybe, we can parse out the relevant constants from the input,
+# and then set up constraints programmatically to enumerate all valid serial
+# numbers.
