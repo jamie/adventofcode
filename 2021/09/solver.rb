12e3c283f02c7b2b253a6b85408291b68a69dc08
Jamie Macey <jamie.git@tracefunc.com>
2022-08-09 18:50:10 -0700

    2021 day 9

--- 2021/09/solver.rb
+++ 2021/09/solver.rb
@@ -1,4 +1,57 @@
 require "advent"
-input = Advent.input
+input = Advent.input.map{_1.split(//).map(&:to_i)}
+
+_input = <<~STR.split("\n").map{_1.split(//).map(&:to_i)}
+  2199943210
+  3987894921
+  9856789892
+  8767896789
+  9899965678
+STR
 
 # Part 1
+
+lowpoints = []
+risk = []
+
+input.size.times do |y|
+  input[0].size.times do |x|
+    cell = input[y][x]
+    left = input[y][x-1] || 99
+    right = input[y][x+1] || 99
+    up = input[y-1][x] rescue 99
+    down = input[y+1][x] rescue 99
+    if [left, right, up, down].min > cell
+      risk << cell + 1
+      lowpoints << [x,y]
+    end
+  end
+end
+
+puts risk.sum
+
+# Part 2
+
+basins = lowpoints.map do |x,y|
+  seen = []
+  to_check = [[x,y]]
+
+  while (pos = to_check.pop)
+    x, y = pos
+    next unless (0..input.size).cover?(y)
+    next unless (0..input[0].size).cover?(x)
+    next if seen.include?(pos)
+    next if (input[y][x] || 9 rescue 9) == 9
+    seen << pos
+
+    to_check << [x+1, y]
+    to_check << [x-1, y]
+    to_check << [x, y+1]
+    to_check << [x, y-1]
+    to_check -= seen
+  end
+
+  seen.size
+end
+
+puts basins.sort.reverse.take(3).inject(&:*)
